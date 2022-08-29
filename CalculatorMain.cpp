3ccf0ef41977adec81137923649589813f4110e4
Jose Arroyo <arroyjose@aim.com>
2022-08-28 19:30:03 -0500

    Added logic for operators. Changed spacing.

--- CalculatorMain.cpp
+++ CalculatorMain.cpp
@@ -6,87 +6,87 @@ CalculatorMain::CalculatorMain(QWidget *parent)
     , ui(new Ui::CalculatorWidget)
     , calculatedValue {}
     , operatorPressed {false}
+    , lastOperator {}
 {
-    ui->setupUi(this);
-
-    connectUIObjects();
+  ui->setupUi(this);
+  connectUIObjects();
 }
 
 void CalculatorMain::connectUIObjects()
 {
-    connect(ui->oneButton, &QPushButton::clicked, this, &CalculatorMain::OnOneButtonClicked);
-    connect(ui->twoButton, &QPushButton::clicked, this, &CalculatorMain::OnTwoButtonClicked);
-    connect(ui->threeButton, &QPushButton::clicked, this, &CalculatorMain::OnThreeButtonClicked);
-    connect(ui->fourButton, &QPushButton::clicked, this, &CalculatorMain::OnFourButtonClicked);
-    connect(ui->fiveButton, &QPushButton::clicked, this, &CalculatorMain::OnFiveButtonClicked);
-    connect(ui->sixButton, &QPushButton::clicked, this, &CalculatorMain::OnSixButtonClicked);
-    connect(ui->sevenButton, &QPushButton::clicked, this, &CalculatorMain::OnSevenButtonClicked);
-    connect(ui->eightButton, &QPushButton::clicked, this, &CalculatorMain::OnEightButtonClicked);
-    connect(ui->nineButton, &QPushButton::clicked, this, &CalculatorMain::OnNineButtonClicked);
-    connect(ui->zeroButton, &QPushButton::clicked, this, &CalculatorMain::OnZeroButtonClicked);
-    connect(ui->plusMinusButton, &QPushButton::clicked, this, &CalculatorMain::OnPlusMinusButtonClicked);
-    connect(ui->decimalPointButton, &QPushButton::clicked, this, &CalculatorMain::OnDecimalPointButtonClicked);
-    connect(ui->equalsButton, &QPushButton::clicked, this, &CalculatorMain::OnEqualsButtonClicked);
-    connect(ui->additionButton, &QPushButton::clicked, this, &CalculatorMain::OnAdditionButtonClicked);
-    connect(ui->subtractionButton, &QPushButton::clicked, this, &CalculatorMain::OnSubtractionButtonClicked);
-    connect(ui->multiplicationButton, &QPushButton::clicked, this, &CalculatorMain::OnMultiplicationButtonClicked);
-    connect(ui->divisionButton, &QPushButton::clicked, this, &CalculatorMain::OnDivisionButtonClicked);
-    connect(ui->clearAllButton, &QPushButton::clicked, this, &CalculatorMain::OnClearAllButtonClicked);
+  connect(ui->oneButton, &QPushButton::clicked, this, &CalculatorMain::OnOneButtonClicked);
+  connect(ui->twoButton, &QPushButton::clicked, this, &CalculatorMain::OnTwoButtonClicked);
+  connect(ui->threeButton, &QPushButton::clicked, this, &CalculatorMain::OnThreeButtonClicked);
+  connect(ui->fourButton, &QPushButton::clicked, this, &CalculatorMain::OnFourButtonClicked);
+  connect(ui->fiveButton, &QPushButton::clicked, this, &CalculatorMain::OnFiveButtonClicked);
+  connect(ui->sixButton, &QPushButton::clicked, this, &CalculatorMain::OnSixButtonClicked);
+  connect(ui->sevenButton, &QPushButton::clicked, this, &CalculatorMain::OnSevenButtonClicked);
+  connect(ui->eightButton, &QPushButton::clicked, this, &CalculatorMain::OnEightButtonClicked);
+  connect(ui->nineButton, &QPushButton::clicked, this, &CalculatorMain::OnNineButtonClicked);
+  connect(ui->zeroButton, &QPushButton::clicked, this, &CalculatorMain::OnZeroButtonClicked);
+  connect(ui->plusMinusButton, &QPushButton::clicked, this, &CalculatorMain::OnPlusMinusButtonClicked);
+  connect(ui->decimalPointButton, &QPushButton::clicked, this, &CalculatorMain::OnDecimalPointButtonClicked);
+  connect(ui->equalsButton, &QPushButton::clicked, this, &CalculatorMain::OnEqualsButtonClicked);
+  connect(ui->additionButton, &QPushButton::clicked, this, &CalculatorMain::OnAdditionButtonClicked);
+  connect(ui->subtractionButton, &QPushButton::clicked, this, &CalculatorMain::OnSubtractionButtonClicked);
+  connect(ui->multiplicationButton, &QPushButton::clicked, this, &CalculatorMain::OnMultiplicationButtonClicked);
+  connect(ui->divisionButton, &QPushButton::clicked, this, &CalculatorMain::OnDivisionButtonClicked);
+  connect(ui->clearAllButton, &QPushButton::clicked, this, &CalculatorMain::OnClearAllButtonClicked);
 }
 
 CalculatorMain::~CalculatorMain()
 {
-    delete ui;
+  delete ui;
 }
 
 void CalculatorMain::OnOneButtonClicked()
 {
-    processNumberInput(QString("1"));
+  processNumberInput(QString("1"));
 }
 
 void CalculatorMain::OnTwoButtonClicked()
 {
-    processNumberInput(QString("2"));
+  processNumberInput(QString("2"));
 }
 
 void CalculatorMain::OnThreeButtonClicked()
 {
-    processNumberInput(QString("3"));
+  processNumberInput(QString("3"));
 }
 
 void CalculatorMain::OnFourButtonClicked()
 {
-    processNumberInput(QString("4"));
+  processNumberInput(QString("4"));
 }
 
 void CalculatorMain::OnFiveButtonClicked()
 {
-    processNumberInput(QString("5"));
+  processNumberInput(QString("5"));
 }
 
 void CalculatorMain::OnSixButtonClicked()
 {
-    processNumberInput(QString("6"));
+  processNumberInput(QString("6"));
 }
 
 void CalculatorMain::OnSevenButtonClicked()
 {
-    processNumberInput(QString("7"));
+  processNumberInput(QString("7"));
 }
 
 void CalculatorMain::OnEightButtonClicked()
 {
-    processNumberInput(QString("8"));
+  processNumberInput(QString("8"));
 }
 
 void CalculatorMain::OnNineButtonClicked()
 {
-    processNumberInput(QString("9"));
+  processNumberInput(QString("9"));
 }
 
 void CalculatorMain::OnZeroButtonClicked()
 {
-    processNumberInput(QString("0"));
+  processNumberInput(QString("0"));
 }
 
 void CalculatorMain::OnPlusMinusButtonClicked()
@@ -101,52 +101,135 @@ void CalculatorMain::OnDecimalPointButtonClicked()
 
 void CalculatorMain::OnEqualsButtonClicked()
 {
-    ui->calculatorNumberLineEdit->setText(QString::number(calculatedValue));
+  double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
+  double total = 0;
+
+  if (operatorPressed)
+  {
+    switch (lastOperator)
+    {
+      case Operator::addition:
+        {
+          total = Add(convertedValue, calculatedValue);
+        }
+        break;
+      case Operator::subtraction:
+        {
+          total = Subtract(convertedValue, calculatedValue);
+        }
+        break;
+      case Operator::multiplication:
+        {
+          total = Multiply(convertedValue, calculatedValue);
+        }
+        break;
+      case Operator::division:
+        {
+          total = Divide(convertedValue, calculatedValue);
+        }
+        break;
+      case Operator::none:
+      default:
+        break;
+    }
+  }
+
+  ui->calculatorNumberLineEdit->setText(QString::number(total));
+  lastOperator = Operator::none;
 }
 
 void CalculatorMain::OnAdditionButtonClicked()
 {
-    double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
-    calculatedValue = calculatedValue + convertedValue;
+  if (operatorPressed == false)
+    {double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
+    calculatedValue = Add(calculatedValue, convertedValue);
+    ui->calculatorNumberLineEdit->setText(QString::number(calculatedValue));
     operatorPressed = true;
+    lastOperator = Operator::addition;
+  }
 }
 
 void CalculatorMain::OnSubtractionButtonClicked()
 {
-
+  if (operatorPressed == false)
+  {
+    double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
+    calculatedValue = Subtract(calculatedValue, convertedValue);
+    ui->calculatorNumberLineEdit->setText(QString::number(calculatedValue));
+    operatorPressed = true;
+    lastOperator = Operator::subtraction;
+  }
 }
 
 void CalculatorMain::OnMultiplicationButtonClicked()
 {
-
+  if (operatorPressed == false)
+    {double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
+    calculatedValue = Multiply(calculatedValue, convertedValue);
+    ui->calculatorNumberLineEdit->setText(QString::number(calculatedValue));
+    operatorPressed = true;
+    lastOperator = Operator::multiplication;
+  }
 }
 
 void CalculatorMain::OnDivisionButtonClicked()
 {
-
+  if (operatorPressed == false)
+    {double convertedValue = ui->calculatorNumberLineEdit->text().toDouble();
+    calculatedValue = Divide(calculatedValue, convertedValue);
+    ui->calculatorNumberLineEdit->setText(QString::number(calculatedValue));
+    operatorPressed = true;
+    lastOperator = Operator::division;
+  }
 }
 
 void CalculatorMain::OnClearAllButtonClicked()
 {
-    ui->calculatorNumberLineEdit->clear();
-    calculatedValue = 0;
-    operatorPressed = false;
+  ui->calculatorNumberLineEdit->clear();
+  calculatedValue = 0;
+  operatorPressed = false;
 }
 
-void CalculatorMain::OnAddButtonClicked()
+double CalculatorMain::Add(double a, double b)
 {
+  return a + b;
+}
 
+double CalculatorMain::Subtract(double a, double b)
+{
+  return a - b;
+}
+
+double CalculatorMain::Multiply(double a, double b)
+{
+  return a * b;
+}
+
+double CalculatorMain::Divide(double a, double b)
+{
+  double val = 0;
+
+  if (b != 0)
+  {
+      val = a / b;
+  }
+  else
+  {
+      // throw exception
+  }
+
+  return val;
 }
 
 void CalculatorMain::processNumberInput(const QString &number)
 {
-    if (operatorPressed == true)
-    {
-        ui->calculatorNumberLineEdit->clear();
-        operatorPressed = false;
-    }
+  if (operatorPressed == true)
+  {
+    ui->calculatorNumberLineEdit->clear();
+  }
 
-    ui->calculatorNumberLineEdit->setText(ui->calculatorNumberLineEdit->text() + number);
+  ui->calculatorNumberLineEdit->setText(ui->calculatorNumberLineEdit->text() + number);
+  operatorPressed = false;
 }
 
 
--- CalculatorMain.h
+++ CalculatorMain.h
@@ -8,41 +8,55 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class CalculatorMain; }
 QT_END_NAMESPACE
 
+enum class Operator
+{
+  none = 0,
+  addition,
+  subtraction,
+  multiplication,
+  division
+};
+
 class CalculatorMain : public QWidget
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    CalculatorMain(QWidget *parent = nullptr);
-    ~CalculatorMain();
+  CalculatorMain(QWidget *parent = nullptr);
+  ~CalculatorMain();
 
 private:
-    Ui::CalculatorWidget *ui;
-    double calculatedValue;
-    bool operatorPressed;
+  Ui::CalculatorWidget *ui;
+  double calculatedValue;
+  bool operatorPressed;
+  Operator lastOperator;
 
-    void connectUIObjects();
-    void processNumberInput(const QString & number);
+  void connectUIObjects();
+  void processNumberInput(const QString & number);
 
 private slots:
-    void OnOneButtonClicked();
-    void OnTwoButtonClicked();
-    void OnThreeButtonClicked();
-    void OnFourButtonClicked();
-    void OnFiveButtonClicked();
-    void OnSixButtonClicked();
-    void OnSevenButtonClicked();
-    void OnEightButtonClicked();
-    void OnNineButtonClicked();
-    void OnZeroButtonClicked();
-    void OnPlusMinusButtonClicked();
-    void OnDecimalPointButtonClicked();
-    void OnEqualsButtonClicked();
-    void OnAdditionButtonClicked();
-    void OnSubtractionButtonClicked();
-    void OnMultiplicationButtonClicked();
-    void OnDivisionButtonClicked();
-    void OnClearAllButtonClicked();
-    void OnAddButtonClicked();
+  void OnOneButtonClicked();
+  void OnTwoButtonClicked();
+  void OnThreeButtonClicked();
+  void OnFourButtonClicked();
+  void OnFiveButtonClicked();
+  void OnSixButtonClicked();
+  void OnSevenButtonClicked();
+  void OnEightButtonClicked();
+  void OnNineButtonClicked();
+  void OnZeroButtonClicked();
+  void OnPlusMinusButtonClicked();
+  void OnDecimalPointButtonClicked();
+  void OnEqualsButtonClicked();
+  void OnAdditionButtonClicked();
+  void OnSubtractionButtonClicked();
+  void OnMultiplicationButtonClicked();
+  void OnDivisionButtonClicked();
+  void OnClearAllButtonClicked();
+
+  double Add(double a, double b);
+  double Subtract(double a, double b);
+  double Multiply(double a, double b);
+  double Divide(double a, double b);
 };
 #endif // WIDGET_H
